c862485651b59ac74a259222b238aaf91e330b4f
Mingjun Geng <sooofeve@gmail.com>
2023-01-27 23:02:40 -0800

    2 way to  <connected to mongooseDB>

--- README.md
+++ README.md
@@ -5,4 +5,6 @@ npm init -y
 npx gitignore node
 npm i express
 touch server.js
+
+npm install mongoose
 ```
--- server.js
+++ server.js
@@ -1,5 +1,32 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+// mongodb+srv://everest:<password>@cluster0.gst1zlc.mongodb.net/?retryWrites=true&w=majority
+
+const connectionString = "mongodb+srv://everest:QAZwsx123@cluster0.gst1zlc.mongodb.net/?retryWrites=true&w=majority";
+
+// way 1 <connected to mongooseDB>
+// mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
+// const db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', function() {
+//   console.log("Connected to MongoDB Atlas");
+// });
+
+// way 2 <connected to mongooseDB>
+async function connect(){
+    try{
+        await mongoose.connect(connectionString,{
+            useNewUrlParser: true, 
+            useUnifiedTopology: true
+        });
+        console.log('connected to mongooseDB');
+    }catch{
+        console.error(error);
+    }
+}
+connect();
 
 app.get('/', (req, res) => {
     res.send('Welcome to the game app!');
